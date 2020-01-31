1ada65165298a2648f3201f86433412c563f4f92
Binod Adhikary <adhikary@medicmobile.org>
2020-02-01 00:13:13 +0545

    Added guessFormat and guessBSYearForPastDate

--- miti.js
+++ miti.js
@@ -1,14 +1,27 @@
 var moment = require('moment');
 var bs = require('bikram-sambat')
 function parseDateFromMessage(message, isBikramSambat) {
-    const dateString = getDateStringFromMessage(message);
-    if (!isBikramSambat) {
-        return moment(dateString).toDate();
+    var resultDate;
+    try {
+        var dateString = getDateStringFromMessage(message);
+        const dateFormat = guessFormat(dateString);
+        if (!isBikramSambat) {
+            resultDate = dateFormat && moment(dateString, dateFormat).toDate();
+        }
+        else {
+            if(dateFormat === "MM DD") {
+                dateString = guessBSYearForPastDate(dateString) + " " + dateString;
+            }
+            const fullDateBSArray = dateString.split(" ");
+            const gregDate = bs.toGreg(fullDateBSArray[0], fullDateBSArray[1], fullDateBSArray[2]);
+            resultDate = new Date(gregDate.year, gregDate.month - 1, gregDate.day);
+        }
+        return resultDate || false;
     }
-    else {
-        const fullDateBSArray = dateString.split(" ");
-        const gregDate = bs.toGreg(fullDateBSArray[0], fullDateBSArray[1], fullDateBSArray[2]);
-        return new Date (gregDate.year, gregDate.month - 1, gregDate.day);
+
+    catch (err) {
+        console.log("Error occurred: " + err.message);
+        return false;
     }
 }
 
@@ -16,4 +29,33 @@ function getDateStringFromMessage(message, position = 3) {
     return message.substring(message.indexOf(" ", position - 1) + 1);
 }
 
-module.exports = { getDateStringFromMessage, parseDateFromMessage };
+//Supported Formats: YYYY MM DD, YYYY M D, MM DD
+function guessFormat(dateString) {
+    const YYYY_mM_dD = /^\d{4}\ \d{1,2}\ \d{1,2}$/;
+    const mM_dD = /^\d{1,2}\ \d{1,2}$/;
+    if (YYYY_mM_dD.test(dateString)) {
+        return "YYYY MM DD";
+    }
+    else if (mM_dD.test(dateString)) {
+        return "MM DD";
+    }
+    return false;
+}
+
+function guessBSYearForPastDate(dateString) {
+    const dateParts = dateString.split(' ');
+    const month = dateParts[0];
+    const day = dateParts[1];
+    const today_8601 = moment().format("YYYY-MM-DD");
+    const today_BS = bs.toBik_euro(today_8601);
+    const currentYear_BS = today_BS.substring(0, 4);
+    const guessedDate = bs.toGreg_text(currentYear_BS, month, day);
+    if(moment().isSameOrAfter(guessedDate)) {
+        return currentYear_BS;
+    }
+    else {
+        return (parseInt(currentYear_BS) - 1).toString();
+    }
+}
+
+module.exports = { getDateStringFromMessage, parseDateFromMessage, guessFormat, guessBSYearForPastDate };
--- test/test.spec.js
+++ test/test.spec.js
@@ -2,6 +2,34 @@ var miti = require('../miti');
 var expect = require('chai').expect;
 var moment = require('moment');
 
+describe("Tests", function () {
+    it("test moment formats", function () {
+        expect(moment('Aug 15', "MMM DD").isSame(moment("2020-08-15").toDate()));
+    });
+});
+
+
+describe("Format Guesser Tests", function () {
+    it("should guess YYYY MM DD correctly", function () {
+        expect(miti.guessFormat("2019 01 01")).to.equal("YYYY MM DD");
+        expect(miti.guessFormat("2019 2 1")).to.equal("YYYY MM DD");
+    });
+    it("should guess MM DD correctly", function() {
+        expect(miti.guessFormat("01 01")).to.equal("MM DD");
+    });
+});
+
+describe("BS Year Guesser Tests", function () {
+    it("should guess this year correctly", function () {
+        expect(miti.guessBSYearForPastDate("01 01")).to.equal("2076")
+        expect(miti.guessBSYearForPastDate("10 15")).to.equal("2076");
+    });
+    it("should guess last year correctly", function() {
+        expect(miti.guessBSYearForPastDate("10 29")).to.equal("2075");
+        expect(miti.guessBSYearForPastDate("12 29")).to.equal("2075");
+    });
+});
+
 describe("Date Parser Tests", function () {
     it("should find date string", function () {
         expect(miti.getDateStringFromMessage("अ 12345 2076 01 02")).to.equal("2076 01 02");
@@ -10,8 +38,15 @@ describe("Date Parser Tests", function () {
         expect(miti.getDateStringFromMessage("A 12345 Jan 1, 2013"), 3).to.equal("Jan 1, 2013");
     });
 
-    it("should return Gregorian Dates", function () {
+    it("should return dates for Bikram Sambat", function () {
         expect(moment("2019-04-15").isSame(miti.parseDateFromMessage("अ 12345 2076 01 02", true))).to.be.true;
+        expect(moment("2019-04-15").isSame(miti.parseDateFromMessage("अ 12345 2076 1 2", true))).to.be.true;
+        expect(moment("2019-04-15").isSame(miti.parseDateFromMessage("अ 12345 1 2", true))).to.be.true;
+    });
+
+    it("should return dates for Gregorian dates", function () {
         expect(moment("2019-01-02").isSame(miti.parseDateFromMessage("अ 12345 2019 01 02", false))).to.be.true;
-    })
+        expect(moment("2019-01-02").isSame(miti.parseDateFromMessage("अ 12345 2019 1 2", false))).to.be.true;
+    });
 });
+
